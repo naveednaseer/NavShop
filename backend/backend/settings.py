c1562abc2bb531d3aff11730b3c1bd8799d18cdf
naveednaseer <nnnaveednaseer0@gmail.com>
2021-06-04 10:10:23 +0530

    cmtt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gdj(l)3kd^x#$och#di*set%k9*8#g^e02-8_*ozb=@+yeww6_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000']
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'phroph.herokuapp.com']
 
 
 # Application definition
