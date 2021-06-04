a1238c3aa84238f618e0cf063eff84daa7979ad5
naveednaseer <nnnaveednaseer0@gmail.com>
2021-06-04 10:00:29 +0530

    deploy_fix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView'
+
+from django.views.static import serve
+from django.conf.urls import url
 
 
 urlpatterns = [
@@ -26,6 +29,10 @@ urlpatterns = [
     path('api/products/', include('base.urls.product_urls')),
     path('api/users/', include('base.urls.user_urls')),
     path('api/orders/', include('base.urls.order_urls')),
+
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 
